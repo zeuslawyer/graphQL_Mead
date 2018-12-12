739ba9161d402f54d389432800f1767cd3db1ffc
zubin | zeuscoder <zubin.pratap@gmail.com>
2018-12-12 17:57:35 +1100

    implement data types for queries. Add README.md as notes to self.

---
+++ basics/README.md
@@ -0,0 +1,18 @@
+#Operations
+Broadly two types - Queries and Mutations.
+
+#typeDefs
+This is how you define the type of __Operations__ and the data types.
+
+Data in GraphQL fall into two broad categories. They are: 
+1) scalar types (store a single, discrete value). 
+2) collections of scalars (objects/arrays)
+
+But they fall into 5 types...
+
+String | Boolean (true/false) | Int | Float | ID (capitals. often are Strings) 
+
+#Resolvers
+1) are objects
+2) have methods as properties
+3) these methods return data, which must conform to the type definitions
--- basics/src/index.js
+++ basics/src/index.js
@@ -10,15 +10,37 @@ import { GraphQLServer } from "graphql-yoga";
 */
 
 const typeDefs = `
-    type Query {            #Query -> operation type. Other types incl mutation
-        hello: String!      #->   always returns a String type
+    type Query {            
+        id: ID!
+        name: String!
+        age: Int!
+        employed: Boolean!
+        gpa: Float    # not null safe - can return EITHER null or Float
     }
 `;
 
+/* 
+    Resolvers (methods that implement operations and fetch/return data). 
+    Is an object with methods as properties
+    follows the schema in type definitions 
+*/
+
 const resolvers = {
   Query: {
-    hello() {
-      return `This is my first successful query`;
+    id() {
+      return "abc19821";
+    },
+    name() {
+      return `Zubin Pratap`;
+    },
+    age() {
+      return 37;
+    },
+    employed() {
+      return false;
+    },
+    gpa() {
+      return null;
     }
   }
 };
@@ -27,8 +49,3 @@ const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => {
   console.log("Server running on default port: 4000");
 });
-
-/* 
-    Resolvers (functions that implement operations). Is an object with function properties
-    follows the schema in type definitions 
-*/
